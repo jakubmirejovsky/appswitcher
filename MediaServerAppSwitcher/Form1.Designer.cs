70e5d7541b5066ba99b6b940af5e14f467ba8b06
JakubM <j.mirejovsky@gmail.com>
2021-08-19 09:07:54 +0200

    Initial commit

---
+++ MediaServerAppSwitcher/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace MediaServerAppSwitcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.stopTimerButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.startTimerButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.timerStatus = new System.Windows.Forms.Label();
+            this.secondsOneLabel = new System.Windows.Forms.Label();
+            this.secondsTwoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // stopTimerButton
+            // 
+            this.stopTimerButton.Enabled = false;
+            this.stopTimerButton.Location = new System.Drawing.Point(264, 130);
+            this.stopTimerButton.Name = "stopTimerButton";
+            this.stopTimerButton.Size = new System.Drawing.Size(75, 23);
+            this.stopTimerButton.TabIndex = 7;
+            this.stopTimerButton.Text = "Stop timer";
+            this.stopTimerButton.UseVisualStyleBackColor = true;
+            this.stopTimerButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(264, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 407);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(138, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 407);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // startTimerButton
+            // 
+            this.startTimerButton.Enabled = false;
+            this.startTimerButton.Location = new System.Drawing.Point(264, 101);
+            this.startTimerButton.Name = "startTimerButton";
+            this.startTimerButton.Size = new System.Drawing.Size(75, 23);
+            this.startTimerButton.TabIndex = 6;
+            this.startTimerButton.Text = "Start timer";
+            this.startTimerButton.UseVisualStyleBackColor = true;
+            this.startTimerButton.Click += new System.EventHandler(this.startTimerButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(264, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(264, 66);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save all";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(346, 71);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(56, 13);
+            this.status.TabIndex = 7;
+            this.status.Text = "Not saved";
+            this.status.Click += new System.EventHandler(this.status_Click);
+            // 
+            // timerStatus
+            // 
+            this.timerStatus.AutoSize = true;
+            this.timerStatus.Location = new System.Drawing.Point(12, 428);
+            this.timerStatus.Name = "timerStatus";
+            this.timerStatus.Size = new System.Drawing.Size(84, 13);
+            this.timerStatus.TabIndex = 8;
+            this.timerStatus.Text = "Timer is stopped";
+            // 
+            // secondsOneLabel
+            // 
+            this.secondsOneLabel.AutoSize = true;
+            this.secondsOneLabel.Location = new System.Drawing.Point(371, 15);
+            this.secondsOneLabel.Name = "secondsOneLabel";
+            this.secondsOneLabel.Size = new System.Drawing.Size(166, 13);
+            this.secondsOneLabel.TabIndex = 9;
+            this.secondsOneLabel.Text = "Duration of first window (seconds)";
+            this.secondsOneLabel.Click += new System.EventHandler(this.secondsOneLabel_Click);
+            // 
+            // secondsTwoLabel
+            // 
+            this.secondsTwoLabel.AutoSize = true;
+            this.secondsTwoLabel.Location = new System.Drawing.Point(371, 42);
+            this.secondsTwoLabel.Name = "secondsTwoLabel";
+            this.secondsTwoLabel.Size = new System.Drawing.Size(185, 13);
+            this.secondsTwoLabel.TabIndex = 10;
+            this.secondsTwoLabel.Text = "Duration of second window (seconds)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.secondsTwoLabel);
+            this.Controls.Add(this.secondsOneLabel);
+            this.Controls.Add(this.timerStatus);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.startTimerButton);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.stopTimerButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button stopTimerButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button startTimerButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Label timerStatus;
+        private System.Windows.Forms.Label secondsOneLabel;
+        private System.Windows.Forms.Label secondsTwoLabel;
+    }
+}
+
---
+++ MediaServerAppSwitcher/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MediaServerAppSwitcher
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer t1;
+        private System.Windows.Forms.Timer t2;
+
+        public bool savedSettings = false;
+
+        public int secondsOne = 2000; // in ms
+        public int secondsTwo = 2000; // in ms
+        public int processIdOne = 0;
+        public int processIdTwo = 0;
+
+        public int activeProcess = 1;
+
+        public DesktopWindow dw1;
+        public DesktopWindow dw2;
+
+        private List<DesktopWindow> listdw;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.listdw = User32Helper.GetDesktopWindows();
+
+            foreach (DesktopWindow d in this.listdw)
+            {
+                if (d.IsVisible)
+                {
+                    listBox1.Items.Add(d);
+                    listBox2.Items.Add(d);
+                }
+            }
+
+            /*
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                if (p.ProcessName != "svchost")
+                {
+                    listBox1.Items.Add(new ProcessItem(p.Id, p.ProcessName));
+                    listBox2.Items.Add(new ProcessItem(p.Id, p.ProcessName));
+                }
+            }
+            */
+
+            this.t1 = new System.Windows.Forms.Timer();
+
+            this.t1.Interval = 1500;
+
+            this.t1.Tick += new EventHandler(timer_Tick);
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            this.t1.Interval = activeProcess == 1 ? this.secondsOne : this.secondsTwo;
+
+            //Call method
+            Debug.WriteLine("constructor fired");
+
+            if (activeProcess == 1)
+            {
+                ProcessHelper.SetFocusToExternalApp(this.dw1.Handle);
+                activeProcess = 2;
+            }
+            else
+            {
+                ProcessHelper.SetFocusToExternalApp(this.dw2.Handle);
+                activeProcess = 1;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timerStatus.Text = "Timer is stopped";
+            this.t1.Stop();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+            //this.processIdOne = ((ProcessItem) lb.SelectedItem).id;
+            this.dw1 = ((DesktopWindow) lb.SelectedItem);
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+            //this.processIdTwo = ((ProcessItem)lb.SelectedItem).id;
+            this.dw2 = ((DesktopWindow)lb.SelectedItem);
+        }
+
+        private void startTimerButton_Click(object sender, EventArgs e)
+        {
+            timerStatus.Text = "Timer is running";
+            this.t1.Start();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Int32.TryParse((sender as TextBox).Text, out this.secondsOne);
+            this.secondsOne *= 1000;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Int32.TryParse((sender as TextBox).Text, out this.secondsTwo);
+            this.secondsTwo *= 1000;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (this.dw1 == null || this.dw2 == null)
+            {
+                status.Text = "Please select both applications from lists.";
+            }
+            else
+            {
+                savedSettings = true;
+
+                startTimerButton.Enabled = true;
+                stopTimerButton.Enabled = true;
+
+                status.Text = "App 1 (" + this.dw1.ToString() + ")¨will be shown for " + (this.secondsOne/1000) +
+                              " seconds and then will " +
+                              "switch to App 2 (" + this.dw2.ToString() + ") for " + (this.secondsTwo/1000) + " seconds.";
+            }
+        }
+
+        private void status_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void secondsOneLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class ProcessHelper
+    {
+        public static void SetFocusToExternalApp(IntPtr ipHwnd)
+        {
+            //Process[] arrProcesses = Process.GetProcessesByName(strProcessName);
+            //Process p = Process.GetProcessById(processId);
+
+            //IntPtr ipHwnd = p.MainWindowHandle;
+            Thread.Sleep(100);
+            SetForegroundWindow(ipHwnd);
+            ShowWindow(ipHwnd, 5);
+
+        }
+
+        //API-declaration
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        //API-declaration
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    }
+
+    class ProcessItem
+    {
+        public int id;
+        public string name;
+
+        public ProcessItem(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+
+    public class DesktopWindow
+    {
+        public IntPtr Handle { get; set; }
+        public string Title { get; set; }
+        public bool IsVisible { get; set; }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+
+    public class User32Helper
+    {
+        public delegate bool EnumDelegate(IntPtr hWnd, int lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText",
+            ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+
+        [DllImport("user32.dll", EntryPoint = "EnumDesktopWindows",
+            ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool EnumDesktopWindows(IntPtr hDesktop, EnumDelegate lpEnumCallbackFunction,
+            IntPtr lParam);
+
+        public static List<DesktopWindow> GetDesktopWindows()
+        {
+            var collection = new List<DesktopWindow>();
+            EnumDelegate filter = delegate (IntPtr hWnd, int lParam)
+            {
+                var result = new StringBuilder(255);
+                GetWindowText(hWnd, result, result.Capacity + 1);
+                string title = result.ToString();
+
+                var isVisible = !string.IsNullOrEmpty(title) && IsWindowVisible(hWnd);
+
+                if (isVisible)
+                collection.Add(new DesktopWindow { Handle = hWnd, Title = title, IsVisible = isVisible });
+
+                return true;
+            };
+
+            EnumDesktopWindows(IntPtr.Zero, filter, IntPtr.Zero);
+            return collection;
+        }
+    }
+}
